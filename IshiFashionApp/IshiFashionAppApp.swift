44c6bce0b0cb52bfad71cfc62039de7084f51696
Ishini Nadeesha <134253245+IshiniNadeesha@users.noreply.github.com>
2024-03-30 19:10:38 +0530

    Initial Commit

---
+++ IshiFashionApp/IshiFashionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IshiFashionAppApp.swift
+//  IshiFashionApp
+//
+//  Created by Sandun Bandara on 2024-03-30.
+//
+
+import SwiftUI
+
+@main
+struct IshiFashionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
